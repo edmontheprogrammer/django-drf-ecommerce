6fe28681b60126f1dcf01716b3378a9998b58a14
Edmon Sebit <edmontheprogrammer@gmail.com>
2023-12-18 05:11:19 -0700

    Setting API Documentation

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     # External Packages
     "rest_framework",
+    "drf_spectacular",
     # Internal Apps
     "drfecommerce.product",
 ]
@@ -131,4 +132,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Configuring REST_FRAMEWORK
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django DRF Ecommerce',
+}
