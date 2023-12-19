885a190c3d9900e1cd8097f031cf9781c0bd1e09
Edmon Sebit <edmontheprogrammer@gmail.com>
2023-12-19 05:18:56 -0700

    Creating the Brand

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -3,8 +3,8 @@ from rest_framework import viewsets
 
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
 # Create your views here.
 
@@ -18,4 +18,16 @@ class CategoryViewSet(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+        A simple Viewset for viewing all categories 
+    """
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
